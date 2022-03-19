2648d3de449fe2539e0953f47d697a5cc524ec26
Piyush Tyagi <piyushktyagi@outlook.com>
2022-03-19 14:40:05 +0530

    Added Address Section

--- projects/registration/src/app/models/hsptmodel.ts
+++ projects/registration/src/app/models/hsptmodel.ts
@@ -1,16 +1,18 @@
-import { HsptContactModel } from "./hsptcontactmodel";
-import { HsptUserModel } from "./hsptUserModel";
-import { HsptMiscellaneousModel } from "./HsptMiscellaneousModel";
-import { FetaureModel } from "./FeatureModel";
+import { HsptContactModel } from './HsptContactModel';
+import { HsptUserModel } from './hsptUserModel';
+import { HsptMiscellaneousModel } from './HsptMiscellaneousModel';
+import { FetaureModel } from './FeatureModel';
+import { HsptAddressModel } from './HsptAddressModel';
 
 export class HsptModel {
-    HospitalName: string = '';
-    HsptContact: HsptContactModel = new HsptContactModel();
-    HsptUser: HsptUserModel = new HsptUserModel();
-    HsptMiscellaneous: HsptMiscellaneousModel = new HsptMiscellaneousModel();
-    HsptProviders: Array<FetaureModel> = new Array<FetaureModel>();
-    HsptFacilities: Array<FetaureModel> = new Array<FetaureModel>();
-    HsptSpecialities: Array<FetaureModel> = new Array<FetaureModel>();
-    HsptInsurances: Array<FetaureModel> = new Array<FetaureModel>();
-    HsptAyush: Array<FetaureModel> = new Array<FetaureModel>();
+  HospitalName: string = '';
+  HsptContact: HsptContactModel = new HsptContactModel();
+  HsptUser: HsptUserModel = new HsptUserModel();
+  HsptMiscellaneous: HsptMiscellaneousModel = new HsptMiscellaneousModel();
+  HsptProviders: Array<FetaureModel> = new Array<FetaureModel>();
+  HsptFacilities: Array<FetaureModel> = new Array<FetaureModel>();
+  HsptSpecialities: Array<FetaureModel> = new Array<FetaureModel>();
+  HsptInsurances: Array<FetaureModel> = new Array<FetaureModel>();
+  HsptAyush: Array<FetaureModel> = new Array<FetaureModel>();
+  HsptAddress: HsptAddressModel = new HsptAddressModel();
 }
