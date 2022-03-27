31c5fa0ef62a19bf9a1b46e78922ab888bc8cfda
gauravyadav <gauravyadav@tsys.com>
2022-03-27 20:43:33 +0530

    Task-7 validation

--- projects/registration/src/app/hsptuserdetail/hsptuserdetail.component.ts
+++ projects/registration/src/app/hsptuserdetail/hsptuserdetail.component.ts
@@ -5,10 +5,9 @@ import { HsptregisterService } from '../services/hsptregister.service';
 import { HsptModel } from '../models/hsptmodel';
 import { HsptContactModel } from '../models/hsptcontactmodel';
 import { HsptMiscellaneousModel } from '../models/HsptMiscellaneousModel';
-import { EmailValidator, FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
 import { CustomValidator } from '../validaters/CustomValidator';
-import { FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { AbstractControlOptions } from '@angular/forms';
 
 @Component({
   selector: 'app-hsptuserdetail',
@@ -19,13 +18,18 @@ export class HsptuserdetailComponent implements OnInit {
 
   hsptModel: HsptModel = new HsptModel();
   UserModel: HsptUserModel = new HsptUserModel();
-  frm = new FormControl();
-
+  errorsList: Array<string> = new Array<string>();
   userDetailsForm = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(3)]],
-    emailId: ['', [CustomValidator.Required, CustomValidator.Email]]
-  });
+    firstName: [this.UserModel.firstName, CustomValidator.Required],
+    lastName: [null],
+    middleName: [null],
+    emailId: [null, [CustomValidator.Required, CustomValidator.Email]],
+    loginId: [null, [CustomValidator.Required]],
+    phone: [null],
+    password: [null, [CustomValidator.Required, CustomValidator.Password]],
+    confirmPassword: [null, { validators: [CustomValidator.Required, CustomValidator.Password] }],
+    otp: [null, [CustomValidator.Required]],
+  }, { validators: CustomValidator.PasswordMatch, updateOn: "blur" });
 
   constructor(private router: Router, private hsptRgisterService: HsptregisterService,
     private fb: FormBuilder) { }
@@ -39,23 +43,46 @@ export class HsptuserdetailComponent implements OnInit {
     this.router.navigate(["hospital-registration", priceModelId]);
   }
 
-  next(): void {
-    alert(this.userDetailsForm.valid);
-    debugger;
-    this.UserModel = this.userDetailsForm.value;
 
-    if (this.userDetailsForm.valid) {
-      this.hsptModel = this.hsptRgisterService.getRegisterModel();
-      this.hsptModel.hsptUsers = new Array<HsptUserModel>();
-      this.hsptModel.hsptUsers.push(this.UserModel);
-      this.hsptModel.hsptContacts = new Array<HsptContactModel>();
-      this.hsptModel.hsptContacts.push(this.hsptModel.hsptContact);
-      this.hsptModel.hsptMiscellaneousList = new Array<HsptMiscellaneousModel>();
-      this.hsptModel.hsptMiscellaneousList.push(this.hsptModel.hsptMiscellaneous);
-      this.hsptRgisterService.register(this.hsptModel).subscribe(data => {
-        var hospitalPk = data;
-        this.router.navigate(["hospital-welcome", hospitalPk]);
+  next(): void {
+    this.UserModel = this.userDetailsForm.value;
+    this.errorsList = new Array<string>();
+    if (!this.userDetailsForm.valid) {
+      Object.keys(this.userDetailsForm.controls).forEach(ctrlKey => {
+        var ctrl = this.userDetailsForm.controls[ctrlKey];
+        var errors: any = ctrl.errors;
+        if (errors) {
+          Object.keys(errors).forEach(errrorKey => {
+            if(this.errorsList.indexOf(errors[errrorKey])<0){
+            this.errorsList.push(errors[errrorKey]);
+            }
+          });
+        }
       });
+
+      var error: any = this.userDetailsForm.errors;
+      Object.keys(error).forEach(er => {
+        var frmEr = error[er];
+        this.errorsList.push(frmEr);
+      });
+
+      return;
     }
+
+    this.hsptModel = this.hsptRgisterService.getRegisterModel();
+    this.hsptModel.hsptUsers = new Array<HsptUserModel>();
+    this.hsptModel.hsptUsers.push(this.UserModel);
+    this.hsptModel.hsptContacts = new Array<HsptContactModel>();
+    this.hsptModel.hsptContacts.push(this.hsptModel.hsptContact);
+    this.hsptModel.hsptMiscellaneousList = new Array<HsptMiscellaneousModel>();
+    this.hsptModel.hsptMiscellaneousList.push(this.hsptModel.hsptMiscellaneous);
+    this.hsptRgisterService.register(this.hsptModel).subscribe(data => {
+      var hospitalPk = data;
+      this.router.navigate(["hospital-welcome", hospitalPk]);
+    });
+  }
+
+  close(): void {
+    this.errorsList = new Array<string>();
   }
 }
--- projects/registration/src/app/validaters/CustomValidator.ts
+++ projects/registration/src/app/validaters/CustomValidator.ts
@@ -1,12 +1,13 @@
 import { FormControl, ValidationErrors } from '@angular/forms';
+import { ValidatorFn } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 export class CustomValidator {
 
-    static Required(c: FormControl): ValidationErrors | null {
-        let Error: ValidationErrors = { "required": "Please provide required values." }
-        debugger;
-        var crlValue: string = c.value;
-        if (crlValue.trim().length <= 0) {
+    static Required(c: FormControl, fieldName?: string): ValidationErrors | null {
+        let Error: ValidationErrors = { "required": "Please  provide required values." }
+        if (c.value == null || (c.value as string).trim().length <= 0) {
             return Error;
         }
         return null;
@@ -17,4 +18,22 @@ export class CustomValidator {
         let Error: ValidationErrors = { "email": "Please provide valid email." }
         return EMAIL_REGEXP.test(c.value) ? null : Error;
     }
+
+    static Password(c: FormControl): ValidationErrors | null {
+        var passwordRegex: string = '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})';
+        var regex: RegExp = new RegExp(passwordRegex);
+        let error: ValidationErrors = { "password": "Please provide a stong password" }
+        return regex.test(c.value) ? null : error;
+    }
+
+    static PasswordMatch(formGroup: FormGroup): ValidationErrors | null {
+        var error: ValidationErrors = { "passwordMatch": `password does not match` }
+        const password = formGroup.get('password')?.value;
+        const confirmPassword = formGroup.get('confirmPassword')?.value;
+        if (password == confirmPassword) {
+            return null;
+        }
+        return error;
+    }
+
 }
